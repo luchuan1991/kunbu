d133c0ad27901870b8675966a93456e658254c97
luchuan1991 <40435590+luchuan1991@users.noreply.github.com>
2023-09-28 11:01:22 +0800

    Update readme.py

--- fenqu/face/readme.py
+++ fenqu/face/readme.py
@@ -1,4 +1,4 @@
 #facexunhuan.py   用途：按比例生成面部分区的rgb，lab，hsv的csv文件
 #get1face_features.py 用途：按特征生成面部分区图片
-#getface_xunhuan.py 用途：按比例生成面部分区的rgb，lab，hsv的csv文件
+#getface_xunhuan.py 用途：按特征生成面部分区的rgb，lab，hsv的csv文件
 #onefacepc.py 用途：按比例生成面部分区的图片
